7c0b651ec373c2ad78dfea7d376d2f475d732a1a
Vikas Gothi <125548865+vikasgothi48@users.noreply.github.com>
2023-04-17 18:15:18 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,10 @@
 import React from 'react'
 import '../styles/App.css';
+import programmingGif from '../assets/programming.gif';
 
 const GIF = () => {
   return (
-    <img className='import-image' height={200}/>
+    <img src={programmingGif} alt='programming-gif' className='import-image' height={200}/>
   )
 }
 
@@ -13,7 +14,7 @@ const App = () => {
     <div id="main">
       <h1>Add images in 2-different ways.</h1>
       <GIF />
-      <img className='url-image' />
+      <img src="https://picsum.photos/id/400/200"className='url-image' alt="A random image" />
     </div>
   )
 }
